4b16870a279efc2bc221fb9a74369eef805500ed
milicagaljak1 <116593979+milicagaljak1@users.noreply.github.com>
2022-12-17 02:26:17 +0100

    Delete members directory

--- members/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Members(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
